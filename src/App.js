c1fb40f0fb6575a1daa3599fa7b92ad8c9200b89
Suyank Saraswat <suyank@antarx.tech>
2020-07-19 17:05:55 +0500

    remove warnings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import TodoInput from './components/TodoInput';
-import logo from './logo.svg';
 import './App.css';
 import './fonts/font-awesome.css';
 
